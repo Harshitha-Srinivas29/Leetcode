c3037e5bce2daf20cdeea5708d51b7f5404bdf85
Harshitha Srinivas <55800824+Harshitha-Srinivas29@users.noreply.github.com>
2025-09-27 17:51:41 -0400

    Time: 3 ms (98.94%), Space: 19.5 MB (22.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        inputStr = re.sub(r'[^a-z0-9]', '',s)
-        reversedStr = inputStr[::-1]
-        return reversedStr == inputStr
+        given_str = re.sub(r'[^a-z0-9]', '', s)
+        reverse_str = given_str[::-1]
+        return given_str == reverse_str
+
+        
