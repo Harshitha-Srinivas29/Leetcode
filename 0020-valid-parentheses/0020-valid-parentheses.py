049e83b273cd3f999970b0c4ac2840ba00d2f39d
Harshitha Srinivas <55800824+Harshitha-Srinivas29@users.noreply.github.com>
2025-09-27 19:38:04 -0400

    Time: 4 ms (11.09%), Space: 18.1 MB (13.37%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,8 +1,8 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        match = {')' : '(', '}' : '{', ']' : '['}
         if len(s) % 2 != 0:
-            return False
+            return False #odd length
+        match = {')' : '(', '}' : '{', ']' : '['}
         stack = []
         for c in s:
             if c in '[({':
@@ -11,5 +11,6 @@ class Solution:
                 if not stack or stack[-1] != match.get(c, None):
                     return False
                 stack.pop()
-        return True
+        return not stack
+        # stack must be empty after returning all the elements
         
