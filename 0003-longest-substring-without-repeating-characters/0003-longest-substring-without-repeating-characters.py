fbc77ada0e6942479717944986c1434051dd2140
Harshitha Srinivas <55800824+Harshitha-Srinivas29@users.noreply.github.com>
2025-09-30 17:34:09 -0400

    Time: 19 ms (53.03%), Space: 18 MB (12.28%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,13 +3,12 @@ class Solution:
         max_count = 0
         seen = set()
         left = 0
+
         for right in range(len(s)):
+            # if char is already in set
             while s[right] in seen:
                 seen.remove(s[left])
                 left += 1
             seen.add(s[right])
             max_count = max(max_count, right-left+1)
         return max_count
-
-
-        
