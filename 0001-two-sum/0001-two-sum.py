ef232e8da46234e9e9d1d6a00660cf5b8ab66516
Harshitha Srinivas <55800824+Harshitha-Srinivas29@users.noreply.github.com>
2025-10-30 01:57:46 -0400

    Time: 0 ms (100%), Space: 18.9 MB (51.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,9 +2,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         map = {}
         for i in range(len(nums)):
-            reminder = target - nums[i]
-            if reminder not in map:
+            rem = target - nums[i]
+            if rem not in map:
                 map[nums[i]] = i
             else:
-                return [i, map[reminder]]
+                return [i, map[rem]]
         return []
